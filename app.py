94f45c49ca8e9f10bf1650563bd8434f0bf20f3b
okubobarros <okubobarros@gmail.com>
2023-01-03 00:51:26 -0300

    display weather data

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+#install requests package
+import requests
+
 api_key = ''
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+#display weather data
+weather = weather_data.json()['weather'][0]['main']
+temp = weather_data.json()['main']['temp']
+
+print(weather, temp)
